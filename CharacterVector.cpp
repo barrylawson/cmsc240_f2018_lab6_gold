539917004e88aa7bc52cb0979652196b7a3c1d0d
mb6wc <mb6wc@mathcs05.richmond.edu>
2018-10-01 10:02:05 -0400

    Added character vector methods

--- CharacterVector.cpp
+++ CharacterVector.cpp
@@ -1,6 +1,8 @@
 #ifndef __CHARACTER_VECTOR_CPP__
 #define __CHARACTER_VECTOR_CPP__
 
+#include <iostream>
+#include <vector>
 #include "IntegerVector.h"
 #include "DoubleVector.h"
 #include "CharacterVector.h"
@@ -10,37 +12,54 @@ CharacterVector::~CharacterVector() {}
 
 int CharacterVector::size() 
 {  
-   return ...
+   return characterVector.size();
 }
 
 // just return the character at the specified index; use the at() method rather
 // than [] so that an out_of_range error will be thrown for an invalid index
 char CharacterVector::get(int index) 
 { 
-   return ...
+   return characterVector.at(index);
 }
 
 // if index is size-legitimate, put the value at that index;
 // otherwise, use push_back to append to the end of the vector
 void CharacterVector::put(char value, int index)
 {
+	if(index <= this->size())
+	{
+		characterVector.at(index) = value;
+	}
+	else
+	{
+		characterVector.push_back(value);
+	}
 }
 
 // use push_back to append
 void CharacterVector::put(char value)
 {
+	characterVector.push_back(value);
 }
 
 // for each integer in integerVector, use static_cast<char> to append as a
 // character to characterVector
 void CharacterVector::appendIntegerVector(IntegerVector& integerVector)
 {
+	for (int i = 0; i < integerVector.size(); i++){
+		this->put(static_cast<char> (integerVector.get(i)));
+	}
 }
 
 // for each double in doubleVector, use static_cast<char> to append as a
 // character to characterVector
 void CharacterVector::appendDoubleVector(DoubleVector& doubleVector)
 {
+	{
+	for (int i = 0; i < doubleVector.size(); i++){
+		this->put(static_cast<char> (doubleVector.get(i)));
+	}
+}
 }
 
 #endif
